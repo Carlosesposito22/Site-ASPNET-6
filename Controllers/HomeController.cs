3303b0ae57871e3774c91d4a1cb43a6ced54ae6a
Carlos E. Espósito <carloseduardoec2004@gmail.com>
2025-07-20 16:49:47 -0300

    Controller e View de List Lanches

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,10 +10,6 @@ namespace site.Controllers
         {
             return View();
         }
-        public IActionResult Demo()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using site.Repositories.Interfaces;
+
+namespace site.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepositoy _lancheRepository;
+
+        public LancheController(ILancheRepositoy lancheRepositoy)
+        {
+            _lancheRepository = lancheRepositoy;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
